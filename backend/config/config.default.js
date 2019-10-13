04509b2e116131edff56c73799f56132ac76ee6d
wangsongsong <wangsongsong@meiliwu.com>
2019-10-13 21:26:17 +0800

    add

--- backend/config/config.default.js
+++ backend/config/config.default.js
@@ -24,7 +24,7 @@ module.exports = appInfo => {
 
   config.mongoose = {
     client: {
-      url: 'mongodb://root:123456@app-mongo:27017/dk?authSource=admin',
+      url: 'mongodb://root:wsskk123456@app-mongo:27017/dk?authSource=admin',
       options: {
         useUnifiedTopology: true,
         useNewUrlParser: true,
